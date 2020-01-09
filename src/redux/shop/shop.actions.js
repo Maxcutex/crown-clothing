b768c9c429bef0b98e8ed6376500d505c55e366f
Eno Bassey <ennyboy@gmail.com>
2020-01-09 12:00:08 +0100

    refactored collection overview, added redux thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,36 @@
 import  
     ShopActionTypes
  from './shop.types';
+import {
+    firestore,
+    convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils.js';
 
-export const updateCollections = collections => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collections
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTION_START,
 });
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTION_SUCCESS, 
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTION_FAILURE,
+    payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () =>{
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+        })
+        .catch(error => dispatch(fetchCollectionsFailure(error.message)))
+        ;
+    }
+
+}
+
